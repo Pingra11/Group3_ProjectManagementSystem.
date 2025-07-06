18beb7073e91c9feda067c16d3b25947b4c4277e
Pingra11 <131923161+Pingra11@users.noreply.github.com>
2025-07-05 22:54:46 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+// Project information data
+const projectInfo = {};
+
+// Array to store requirements
+const requirements = [];
+
+// Array to store time logs
+const timeLogs = [];
+
+// Placeholder functions
+function addProjectInfo() {
+  console.log("Add project info function called");
+}
+
+function addRequirement() {
+  console.log("Add requirement function called");
+}
+
+function logTime() {
+  console.log("Log time function called");
+}
